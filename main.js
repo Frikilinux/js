230229114743553639411f04aea130bc29c8544c
Frikilinux <frikilinux@gmail.com>
2022-11-12 18:50:18 -0300

    Add pizzas to array of objects

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
 // Array de objetos
 
-pizzas = [
+const pizzas = [
   {
     id: 1,
-    nombre: 'Napolitana',
-    ingredientes: ['Queso'],
-    precio: 3000,
+    nombre: 'Margheritta',
+    ingredientes: ['Mozzarella', 'Albahaca', 'Tomates'],
+    precio: 2650,
   },
+  
+  {
+    id: 2,
+    nombre: 'Cuatro Quesos',
+    ingredientes: ['Mozzarella', 'Gorgonzola', 'Parmesano', 'Fontina'],
+    precio: 2900,
+  },
+  
+  {
+    id: 3,
+    nombre: 'Pepperoni',
+    ingredientes: [ 'Pepperoni', 'Mozzarella', 'Albahaca', 'Orégano'],
+    precio: 3200,
+  },
+  
+  {
+    id: 4,
+    nombre: 'Funghi',
+    ingredientes: ['Parmezano', 'Chapiñones', 'Tomate', 'Orégano' ],
+    precio: 3600,
+  },
+  
+  {
+    id: 5,
+    nombre: 'Hawainana',
+    ingredientes: ['Mozzarella', 'Ananá', 'Tomate', 'Jamón'],
+    precio: 2800,
+  },
+  
+  {
+    id: 6,
+    nombre: 'Marinara',
+    ingredientes: ['Cebollas', 'Tomate', 'Ajo', 'Aceite de oliva'],
+    precio: 2100,
+  },
+  
 ]
-
-console.log(pizzas);
