4b44a11fb8194c818ab193744ed53ad6586af217
Frikilinux <frikilinux@gmail.com>
2022-12-10 22:48:56 -0300

    Clean code

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ const pizzas = [
     nombre:'Napolitana',
     ingredientes:['Mozzarella', 'Tomate', 'Albahaca'],
     precio: 1600,
-},
+  },
 ];
 
 let messageContainer = document.querySelector('.msg-container')
@@ -56,43 +56,38 @@ let formInput = document.querySelector('#search')
 let form = document.querySelector('.form');
 
 const searchPizza = (array, id) => {
-  return array.filter((pizza) => pizza.id == id);
+  return array.find((pizza) => pizza.id == id);
 };
 
 const checkAvailId = (value) => value >= 1 && value <= pizzas.length;
 
 const renderPizza = (pizza, input) => {
   formInput.parentElement.style.backgroundColor = 'rgb(28, 69, 21)';
-  messageContainer.innerHTML = `<h2 class="fa-beat">Pizza ${pizza[0].nombre}</h2>
-  <h3 class="fa-beat">$ ${pizza[0].precio}</i></h3>`;
-  formInput.setAttribute('placeholder', `${input}`),
-  form.reset()
+  messageContainer.innerHTML = `<h2 class="fa-beat">Pizza ${pizza.nombre}
+      </h2> <h3 class="fa-beat">$ ${pizza.precio}</i></h3>`;
+  formInput.setAttribute('placeholder', `${input}`);
+  form.reset();
 };
 
 const renderError = (msg) => {
   messageContainer.innerHTML = msg
   formInput.parentElement.style.backgroundColor = 'rgb(80, 23, 23)';
   messageContainer.querySelector('h2').classList.add('error');
-  formInput.setAttribute('placeholder', `#ID`)
+  formInput.setAttribute('placeholder', `#ID`);
   form.reset();
 };
 
 const error = (error) => {
-  switch (error) {
-  case 'notfound':
-    renderError(`<h2>No se encontró el ID, probá con uno
-        entre 1 y ${pizzas.length}</h2>`);
-    break;
-  case 'notNumber':
-    renderError(`<h2>Ingresá un número, please...</h2>`);
-    break;
+  if (error == 'notFound') renderError(`<h2>No se encontró el ID, probá con uno
+      entre 1 y ${pizzas.length}</h2>`);
+  if (error == 'notNumber') renderError(`<h2>Ingresá un número, please...
+      </h2>`);
   };
-};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   let input = formInput.value;
   input === '' ? error('notNumber')
-    : !checkAvailId(input) ? error('notfound')
+    : !checkAvailId(input) ? error('notFound')
       : renderPizza(searchPizza(pizzas, input), input)
 });
